50e1c455e8ad743d15a2045c9f11eae18c22c77e
Paulina Gonzalez C <pgc.lacroix@gmail.com>
2017-05-25 18:54:17 -0400

    se agregan iconos y se mejoran algunos detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,25 +21,26 @@ function suscripcion (){
 
 // juego
 
-var btnGame = document.getElementById("btn-start");
-	btnGame.addEventListener("click", crearMatriz);
+var btnStart = document.getElementById("btn-start");
+	btnStart.addEventListener("click", crearMatriz);
 
 function crearMatriz() {
 	var cooX = document.getElementById("x-coordenate").value;
 	var cooY = document.getElementById("y-coordenate").value;
 
 	if(cooX=="" || cooY == ""){
-	alert("Ingrese los campos requeridos")
+	alert("Ingrese las coodenadas de X e Y")
 	}
 	else if (/^[a-zA-Z]*$/.test(cooX) || /^[a-zA-Z]*$/.test(cooY)) {
 	alert("Ingrese solo numeros")
 	}
 	else if (cooX>10 || cooY >6) {
-	alert("Las coordenadas de X no puede exceder a 10. Las coordenadas de Y no puede exceder a 6")
+	alert("Coordenadas X: El valor debe ser entre 0 y 9." + "<br>" + 
+	      "Coordenadas Y: El valor debe ser entre 0 y 5.")
 	}
 	else {
 		//alargar la caja gris
-		document.getElementById("cajaGame").style.height = "500px";
+		document.getElementById("cajaGame").style.height = "430px";
 		//esconder los input
 		document.getElementById("x-coordenate").style.display = "none";
 		document.getElementById("y-coordenate").style.display = "none";
@@ -49,12 +50,6 @@ function crearMatriz() {
 		//mover el footer grande y el chico
 		document.getElementById("footerGrande").style.top = "1420px";
 		document.getElementById("footerChico").style.top = "1600px";
-
-
 	}
-
-
 }
-
-
 
